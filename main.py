85f96dc2b9542387131f236a0b2c4d151b118c65
Zhorkin Igor <zhorkinigor@yandex.ru>
2021-12-08 17:16:18 +0700

    Git и желтые окружности

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow,QPushButton,QWidget
+from PyQt5 import QtGui
+from PyQt5 import uic
+from PyQt5.QtCore import QRect, Qt
+from PyQt5.QtGui import QPainter,QBrush, QPen
+from random import randint
 
 
-if __name__ == '__main__':
-    main()
+class Form(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('git2.ui', self)
+        self.pushButton.clicked.connect(self.paintcircle)
+        self.should_paint_circle = False
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        if self.should_paint_circle:
+            painter = QtGui.QPainter(self)
+            painter.setRenderHint(QPainter.Antialiasing)
+            painter.setPen(QPen(Qt.yellow, 5, Qt.SolidLine))
+            diametr = randint(0, 300)
+            painter.drawEllipse(randint(0, 700), randint(0, 500), diametr, diametr)
+
+    def paintcircle(self):
+        self.should_paint_circle = True
+        self.update()
+
+
+app = QApplication(sys.argv)
+ex = Form()
+ex.show()
+sys.exit(app.exec_())
